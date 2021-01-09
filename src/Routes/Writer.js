48df0a1a60462cc9a5fc67c680c87abb166ca332
Woony <clover7kso@naver.com>
2021-01-09 16:02:19 +0900

    Finish with CKEditor

--- src/Routes/Writer.js
+++ src/Routes/Writer.js
@@ -1,8 +1,100 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
+import ImgurUploaderInit from "ckeditor5-imgur-uploader";
 
-export default class SimpleImageEditor extends Component {
-  render() {
-    return <div>야발다시진행해보자</div>;
-  }
-}
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
+import Autosave from "@ckeditor/ckeditor5-autosave/src/autosave.js";
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
+import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily.js";
+import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
+import Image from "@ckeditor/ckeditor5-image/src/image.js";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock.js";
+import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed.js";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
+import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
+
+const installedPlugins = [
+  Alignment,
+  Autoformat,
+  Autosave,
+  BlockQuote,
+  Essentials,
+  FontColor,
+  FontFamily,
+  FontSize,
+  Heading,
+  HorizontalLine,
+  Image,
+  ImageCaption,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Link,
+  MediaEmbed,
+  Paragraph,
+  PasteFromOffice,
+  Strikethrough,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  Underline,
+];
+
+export default () => {
+  const ImgurUploader = ImgurUploaderInit({ clientID: "818d43b4be21dd8" });
+
+  return (
+    <div>
+      <CKEditor
+        editor={ClassicEditor}
+        config={{
+          extraPlugins: [ImgurUploader],
+          plugins: [...installedPlugins],
+          toolbar: [
+            "fontFamily",
+            "fontSize",
+            "fontColor",
+            "alignment",
+            "|",
+            "strikethrough",
+            "underline",
+            "horizontalLine",
+            "|",
+            "indent",
+            "outdent",
+            "|",
+            "link",
+            "blockQuote",
+            "imageUpload",
+            "insertTable",
+            "mediaEmbed",
+            "|",
+            "undo",
+            "redo",
+          ],
+        }}
+      />
+    </div>
+  );
+};

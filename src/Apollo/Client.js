bf2368aa69ef5a9136250f71baedff655e78c913
Woony <clover7kso@naver.com>
2021-01-19 02:41:16 +0900

    Deploy Server

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -5,7 +5,9 @@ const cache = new InMemoryCache();
 export default new ApolloClient({
   cache: cache,
   uri:
-    process.env.NODE_ENV === "development" ? "/" : "http://15.165.249.108:4000",
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "http://15.165.249.108:4000",
   clientState: {
     defaults,
     resolvers,

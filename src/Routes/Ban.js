66c42af211d3a52c4560b60e39fd6728c5d39522
Woony <clover7kso@naver.com>
2021-02-08 02:09:43 +0900

    Finishing with ban

--- src/Routes/Ban.js
+++ src/Routes/Ban.js
@@ -22,19 +22,19 @@ const Content = styled.span`
 const getReason = (reason) => {
   if (reason === "NOPE") return "현재 제제사유 검토중입니다.";
   else if (reason === "SEXSURE")
-    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 선정성에 위반되어 제재를 하게 되었습니다.\n\n밴이 풀리는 날짜는 아래와 같이 확인이 가능합니다.\n\n감사힙니다.";
+    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 선정성에 위반되어 제재를 하게 되었습니다.\n밴이 풀리는 날짜는 위와 같이 확인이 가능합니다.";
   else if (reason === "ANTI")
-    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 안티성에 위반되어 제재를 하게 되었습니다.\n\n밴이 풀리는 날짜는 아래와 같이 확인이 가능합니다.\n\n감사힙니다.";
+    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 안티성에 위반되어 제재를 하게 되었습니다.\n밴이 풀리는 날짜는 위와 같이 확인이 가능합니다.";
   else if (reason === "GORE")
-    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 혐오성에 위반되어 제재를 하게 되었습니다.\n\n밴이 풀리는 날짜는 아래와 같이 확인이 가능합니다.\n\n감사힙니다.";
+    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 혐오성에 위반되어 제재를 하게 되었습니다.\n밴이 풀리는 날짜는 위와 같이 확인이 가능합니다.";
   else if (reason === "COMMERCE")
-    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 상업성에 위반되어 제재를 하게 되었습니다.\n\n밴이 풀리는 날짜는 아래와 같이 확인이 가능합니다.\n\n감사힙니다.";
+    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 상업성에 위반되어 제재를 하게 되었습니다.\n밴이 풀리는 날짜는 위와 같이 확인이 가능합니다.";
   else if (reason === "SPAM")
-    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 도배성에 위반되어 제재를 하게 되었습니다.\n\n밴이 풀리는 날짜는 아래와 같이 확인이 가능합니다.\n\n감사힙니다.";
+    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 도배성에 위반되어 제재를 하게 되었습니다.\n밴이 풀리는 날짜는 위와 같이 확인이 가능합니다.";
   else if (reason === "ILLEGAL")
-    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 범법성에 위반되어 제재를 하게 되었습니다.\n\n밴이 풀리는 날짜는 아래와 같이 확인이 가능합니다.\n\n감사힙니다.";
+    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 범법성에 위반되어 제재를 하게 되었습니다.\n밴이 풀리는 날짜는 위와 같이 확인이 가능합니다.";
   else if (reason === "FRIEND")
-    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 친목성에 위반되어 제재를 하게 되었습니다.\n\n커뮤니티 내에서의 친목을 하는 것은 좋으나 과도한 과시 및 추종은 자제 부탁드리겠습니다 :)\n\n밴이 풀리는 날짜는 아래와 같이 확인이 가능합니다.\n\n감사힙니다.";
+    return "안녕하세요 리얼시티 회원님 'CM시티' 입니다.\n회원님이 작성하신 글(댓글)이 리얼시티 커뮤니티 가이드라인인 친목성에 위반되어 제재를 하게 되었습니다.\n커뮤니티 내에서의 친목을 하는 것은 좋으나 과도한 과시 및 추종은 자제 부탁드리겠습니다 :)\n밴이 풀리는 날짜는 위와 같이 확인이 가능합니다.";
   else if (reason === "OTHERS")
     return "기타 다른 이유 때문에 제제당하였습니다.";
 };
@@ -49,6 +49,36 @@ const getDate = (date) => {
   else if (date === "FOREVER") return "영구벤";
 };
 
+function printDate(inputDate) {
+  const today = inputDate;
+  const dayNames = [
+    "(일요일)",
+    "(월요일)",
+    "(화요일)",
+    "(수요일)",
+    "(목요일)",
+    "(금요일)",
+    "(토요일)",
+  ];
+  // getDay: 해당 요일(0 ~ 6)를 나타내는 정수를 반환한다.
+  const day = dayNames[today.getDay()];
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+  const ampm = hour >= 12 ? "PM" : "AM";
+  // 12시간제로 변경
+  hour %= 12;
+  hour = hour || 12; // 0 => 12
+  // 10미만인 분과 초를 2자리로 변경
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
+  const now = `${year}년 ${month}월 ${date}일 ${day} ${hour}:${minute}:${second} ${ampm}`;
+  return now;
+}
+
 const Ban = ({ history }) => {
   const info = history.location.search.replace("?", "").split(",");
   const id = info[0];
@@ -56,11 +86,13 @@ const Ban = ({ history }) => {
   const reason = getReason(info[2]);
   const long = getDate(info[1]);
   const date = info[4];
+  const dateTransform = new Date(date);
+  console.log(printDate(dateTransform));
   return (
     <Wrapper>
       <Title>닉네임 : {id}</Title>
-      <Title>제제일 : {date}</Title>
-      <Title>제제형량 : {long}</Title>
+      <Title>제재일 : {printDate(dateTransform)}</Title>
+      <Title>제재형량 : {long}</Title>
       <Title>누적제제 : {number}</Title>
       <Title>제제사유 : </Title>
       {reason !== undefined
@@ -73,6 +105,8 @@ const Ban = ({ history }) => {
             );
           })
         : null}
+      <Content>밴은 재제 형량를 채운 후 00시 00분에 해제됩니다.</Content>
+      <Content>감사합니다.</Content>
     </Wrapper>
   );
 };
